a461b814360f3898d18a3e7fa3cecab3b9c54378
Ruairi Hayes <RUAIRIH5@ie.ibm.com>
2023-08-16 19:46:58 +0100

    Add inputs

--- dist/index.js
+++ dist/index.js
@@ -2842,13 +2842,23 @@ const wait = __nccwpck_require__(258);
 async function run() {
   try {
     const ms = core.getInput('milliseconds');
+    const apihost = core.getInput('api-host');
+    const apikey = core.getInput('api-key');
+    const porg = core.getInput('provider-org');
+    const apifile = core.getInput('api-file');
+
     core.info(`Waiting ${ms} milliseconds ...`);
+    core.info(`apihost ${apihost}`);
+    core.info(`apikey hegdsqvu${apikey}qsfb£sacghd`);
+    core.info(`porg ${porg}`);
+    core.info(`apifile ${apifile}`);
 
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
+    core.setOutput('action-result', ":satisfied: Everything has run without a hitch. :bowtie:");
   } catch (error) {
     core.setFailed(error.message);
   }
